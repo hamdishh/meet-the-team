8d28ac3e17d74b579fe40be769e83868407538e0
Hamdi <hamdishh2003@gmail.com>
2024-01-30 19:21:12 +0000

    created an array to store team members'

---
+++ starter/index.js
@@ -0,0 +1,17 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./src/page-template.js");
+//create an array to store team members
+const teamMembers = [];
+
+
+// TODO: Write Code to gather information about the development team members, and render the HTML file.
+
