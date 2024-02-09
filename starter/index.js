e9626f5c9f55060d432cbfc65ff8af00dbc7cab7
Hamdi <hamdishh2003@gmail.com>
2024-02-09 03:00:13 +0000

    fixed syntax errors in index.js

--- starter/index.js
+++ starter/index.js
@@ -10,8 +10,6 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
-
-
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 //create an array to store team members
 const teamMembers = [];
@@ -36,11 +34,11 @@ function promptTeamManager() {
         },
         {
             type: "Input", 
-            message: "Enter the team managers office number?:",
+            message: "Enter the team manager's office number?:",
             name: "officeNumber",
         },
     ]).then((answers) => {
-        //drawingg out answers from userninput
+        //drawing out answers from user input
         const { name, id, email, officeNumber } = answers;
 
         //making a manager object with information provided in the user input
@@ -52,86 +50,100 @@ function promptTeamManager() {
         //now call a function to prompt the user for the team engineer's details
         function promptTeamEngineer() {
             inquirer.prompt([
-            {
-                type: "input",
-                message: "Enter the team Engineer's name:",
-                name: "name", 
-            },
-            {
-                type: "input",
-                message: "Enter the team engineers ID:",
-                name: "id",
-            },
-            {
-                type: "input",
-                message: "Enter the team engineers email address",
-                name: "email",
-            },
-            {
-                type: "Input", 
-                message: "Enter the team engineer's github username?:",
-                name: "github",
-            },
+                {
+                    type: "input",
+                    message: "Enter the team Engineer's name:",
+                    name: "name", 
+                },
+                {
+                    type: "input",
+                    message: "Enter the team engineer's ID:",
+                    name: "id",
+                },
+                {
+                    type: "input",
+                    message: "Enter the team engineer's email address",
+                    name: "email",
+                },
+                {
+                    type: "Input", 
+                    message: "Enter the team engineer's github username?:",
+                    name: "github",
+                },
             ]).then((answers) => {
-                //destructing to extract values from 'answers parameter
-                const { name, id, email, github };
+                //destructing to extract values from 'answers' parameter
+                const { name, id, email, github } = answers;
 
-                //creating an engineer object containing all the infor provided by user input
+                //creating an engineer object containing all the info provided by user input
                 const engineer = new Engineer(name, id, email, github);
 
-                //Using push function to add the new "engineer" object to the TeamMembers array alongisde "manager"
+                //Using push function to add the new "engineer" object to the TeamMembers array alongside "manager"
                 teamMembers.push(engineer);
 
-                //calling function to prompt user for Interns details
-                function promptIntern() {
-                    inquirer.prompt([
-                        {
-                        type: "input",
-                        message: "Enter the name of your intern:",
-                        name: "name",
-                        },
-                        {
-                        type: "input",
-                        message: "Enter the intern's ID:",
-                        name: "id",
-                        },
-                        {
-                          type: "input",
-                            message: "Enter the intern's email:",
-                            name: "email",
-                        },
-                        {
-                            type: "input",
-                        message: "Enter the intern's school:",
-                        name: "school",
-                        },
-                    ]).then((answers) {
-                        const { name, id, email, school } = answers;
+                //calling function to prompt user for Intern's details
+                promptIntern();
+            });
+        }
 
-                        //make an intern object with the provided info
-                        const intern = new Intern(name, id, email, school);
+        // Function to prompt the user to add more team members or finish
+        function promptIntern() {
+            inquirer.prompt([
+                {
+                    type: "input",
+                    message: "Enter the name of your intern:",
+                    name: "name",
+                },
+                {
+                    type: "input",
+                    message: "Enter the intern's ID:",
+                    name: "id",
+                },
+                {
+                    type: "input",
+                    message: "Enter the intern's email:",
+                    name: "email",
+                },
+                {
+                    type: "input",
+                    message: "Enter the intern's school:",
+                    name: "school",
+                },
+            ]).then((answers) => {
+                const { name, id, email, school } = answers;
 
-                        //push the intern object to add into the teamMembers array
-                        teamMembers.push(intern);
+                //make an intern object with the provided info
+                const intern = new Intern(name, id, email, school);
 
-                        // add prompt incase user wants to add more team members
-                        promptAddMoreTeamMembers();
-                    });
-                  }
-                // Function to prompt the user to add more team members or finish
-               function promptAddMoreTeamMembers() {
-               inquirer.prompt([
-                        {
-                      type: "list",
-                       message: "Do you want to add more team members?",
-                       choices: ["Add Engineer", "Add Intern", "Finish Building Team"],
-                       name: "choice",
-                      },
-                      ]).then((answer) => {
-                        const { choice } answer;
-            }
-            )
-    }
-    )
+                //push the intern object to add into the teamMembers array
+                teamMembers.push(intern);
 
+                // add prompt in case user wants to add more team members
+                promptAddMoreTeamMembers();
+            });
+        }
+
+        // Function to prompt the user to add more team members or finish
+        function promptAddMoreTeamMembers() {
+            inquirer.prompt([
+                {
+                    type: "list",
+                    message: "Do you want to add more team members?",
+                    choices: ["Add Engineer", "Add Intern", "Finish Building Team"],
+                    name: "choice",
+                },
+            ]).then((answer) => {
+                const { choice } = answer; 
+                if (choice === "Add Engineer") {
+                    promptTeamEngineer();
+                } else if (choice === "Add Intern") {
+                    promptIntern(); 
+                } else {
+                    generateTeamHtml();
+                }
+            });
+        }
+
+        // Call the function to prompt for adding the first team member
+        promptTeamEngineer();
+    });
 }
