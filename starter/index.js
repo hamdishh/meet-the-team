3810a119e2d8dc47ab85b1be1e78c043573e9098
Hamdi <hamdishh2003@gmail.com>
2024-02-16 01:27:47 +0000

    adding if/else for validation

--- starter/index.js
+++ starter/index.js
@@ -2,6 +2,7 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
+
 const path = require("path");
 const fs = require("fs");
 
@@ -12,138 +13,16 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 //create an array to store team members
-const teamMembers = [];
+const teamArray = [];
 
-//I need to create a function to prompt the user for team managers details
-function promptTeamManager() {
-    inquirer.prompt([
-        {
-            type: "input",
-            message: "Enter the team's manager's name:",
-            name: "name",
-        },
-        {
-            type: "input",
-            message: "Enter the team manager's employer ID:",
-            name: "id",
-        },
-        {
-            type: "input",
-            message: "Enter the team manager's email address",
-            name: "email",
-        },
-        {
-            type: "Input", 
-            message: "Enter the team manager's office number?:",
-            name: "officeNumber",
-        },
-    ]).then((answers) => {
-        //drawing out answers from user input
-        const { name, id, email, officeNumber } = answers;
-
-        //making a manager object with information provided in the user input
-        const manager = new Manager(name, id, email, officeNumber);
-
-        //Push the manager object using push function to add into TeamMembers array
-        teamMembers.push(manager);
-
-        //now call a function to prompt the user for the team engineer's details
-        function promptTeamEngineer() {
-            inquirer.prompt([
-                {
-                    type: "input",
-                    message: "Enter the team Engineer's name:",
-                    name: "name", 
-                },
-                {
-                    type: "input",
-                    message: "Enter the team engineer's ID:",
-                    name: "id",
-                },
-                {
-                    type: "input",
-                    message: "Enter the team engineer's email address",
-                    name: "email",
-                },
-                {
-                    type: "Input", 
-                    message: "Enter the team engineer's github username?:",
-                    name: "github",
-                },
-            ]).then((answers) => {
-                //destructing to extract values from 'answers' parameter
-                const { name, id, email, github } = answers;
-
-                //creating an engineer object containing all the info provided by user input
-                const engineer = new Engineer(name, id, email, github);
-
-                //Using push function to add the new "engineer" object to the TeamMembers array alongside "manager"
-                teamMembers.push(engineer);
-
-                //calling function to prompt user for Intern's details
-                promptIntern();
-            });
-        }
-
-        // Function to prompt the user to add more team members or finish
-        function promptIntern() {
-            inquirer.prompt([
-                {
-                    type: "input",
-                    message: "Enter the name of your intern:",
-                    name: "name",
-                },
-                {
-                    type: "input",
-                    message: "Enter the intern's ID:",
-                    name: "id",
-                },
-                {
-                    type: "input",
-                    message: "Enter the intern's email:",
-                    name: "email",
-                },
-                {
-                    type: "input",
-                    message: "Enter the intern's school:",
-                    name: "school",
-                },
-            ]).then((answers) => {
-                const { name, id, email, school } = answers;
-
-                //make an intern object with the provided info
-                const intern = new Intern(name, id, email, school);
-
-                //push the intern object to add into the teamMembers array
-                teamMembers.push(intern);
-
-                // add prompt in case user wants to add more team members
-                promptAddMoreTeamMembers();
-            });
-        }
-
-        // Function to prompt the user to add more team members or finish
-        function promptAddMoreTeamMembers() {
-            inquirer.prompt([
-                {
-                    type: "list",
-                    message: "Do you want to add more team members?",
-                    choices: ["Add Engineer", "Add Intern", "Finish Building Team"],
-                    name: "choice",
-                },
-            ]).then((answer) => {
-                const { choice } = answer; 
-                if (choice === "Add Engineer") {
-                    promptTeamEngineer();
-                } else if (choice === "Add Intern") {
-                    promptIntern(); 
-                } else {
-                    generateTeamHtml();
-                }
-            });
-        }
-
-        // Call the function to prompt for adding the first team member
-        promptTeamEngineer();
-    });
+//prompt user for manager input details
+const inputManager = () => {
+    return inquirer
+    .prompt([
+     {
+        type: "input",
+        name: "name",
+        message: "Please input your team manager's name:",
+        //going with simple if/else statmeents for validation
+     }])
 }
--- starter/lib/Manager.js
+++ starter/lib/Manager.js
@@ -1,6 +1,2 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
-//importing employee class
-const Employee = require("./Employee");
-
-//need to define the manager class thats being inherited from employee
-
+// Importing Employee class
